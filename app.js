0748dc25c33778e1308076de087a26cc57791aed
angad <angadsharma1016@gmail.com>
2018-10-17 17:01:30 +0530

    structured

--- app.js
+++ app.js
@@ -12,30 +12,27 @@ let func = async ()=>{
     let driver = await new Builder().forBrowser("firefox").build();
 
     try{
+        // login
         await driver.get(process.env.URL);
         await driver.findElement({id:"loginid"}).sendKeys(process.env.EMAIL);
         await driver.findElement({id:"logpassword"}).sendKeys(process.env.PASSWORD);
         let arr = await driver.findElements({className:"col-md-3"});
         arr = await arr[arr.length-2].getAttribute("innerHTML");
         arr = arr.split('>')[1].split("<")[0];
-
         await driver.findElement({id:"captchacode"}).sendKeys(arr);
-
         await driver.findElement({name:"frmSubmit"}).click();
 
 
-
+        // go to post attendance page
         await driver.get(process.env.EVENT_PARTICIPANTS_URL);
-
         arr = await driver.findElements({name:"frmSubmit"});
         await arr[1].click();
 
+
+
+
+        // voluntary wait
         await driver.wait(until.titleContains("google"))
-
-        // await arr[0].click();
-
-        // arr = await driver.findElements(By.tagName("button"));
-        // await arr[1].click()
 
     } finally {
         await driver.quit();
